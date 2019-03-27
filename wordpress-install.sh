76d6809ab6f2064b3ebfd0702d5467e59dfced2d
thecuriosky <48576841+thecuriosky@users.noreply.github.com>
2019-03-28 01:41:07 +0530

    Update wordpress-install.sh

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -1,1 +1,16 @@
-#Check if PHP, Mysql & Nginx are installed. If not present, install the missing packages
+#Create a sudo user on server
+echo "Before we begin setting up Server and installing Wordpress CMS,"
+echo "Do you want to create a new user with few root priviledges [y/n]?"
+read -e newUser
+if [ $newUser = y ]; then
+echo "Please provide your root password"
+su
+echo "Now that we are logged in as root user, let's create a New User"
+echo "Enter new userID"
+read -e dummy
+adduser $dummy
+#usermod -aG sudo $dummy
+else
+echo "exiting"
+exit
+fi
