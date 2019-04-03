2882f25e8ddce0f3ae83a0729b7907a3145584f2
thecuriosky <48576841+thecuriosky@users.noreply.github.com>
2019-04-04 00:16:07 +0530

    Update wordpress-install.sh

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -1,16 +1,51 @@
-#Create a sudo user on server
-echo "Before we begin setting up Server and installing Wordpress CMS,"
-echo "Do you want to create a new user with few root priviledges [y/n]?"
-read -e newUser
-if [ $newUser = y ]; then
-echo "Please provide your root password"
-su
-echo "Now that we are logged in as root user, let's create a New User"
-echo "Enter new userID"
-read -e dummy
-adduser $dummy
-#usermod -aG sudo $dummy
+#!/bin/bash
+#Check if PHP, Mysql & Nginx are installed. If not present, install the missing packages
+
+for i in nginx mysql php
+do
+dpkg --get-selections | grep $i
+if [ `echo $?` -eq 0 ]
+then
+echo "+----------------------------------------+"
+echo -e "$i is already installed on server"
+echo "+----------------------------------------+"
 else
-echo "exiting"
-exit
+echo "+------------------------------------+"
+echo -e "$i is not installed on server"
+echo "           Installing $i            " 
+echo "+------------------------------------+"
+sudo apt-get -y update
+sudo apt-get install -y $i
 fi
+done
+
+#Adding domain to hosts
+
+#read ip from ipconfig and storing in variable 'ip' -- ip=$(command to fetch the ip from ifconfig)
+ip=$(ifconfig)
+echo "please enter your desired domain name"
+read domain
+
+ip=$(ifconfig)
+#d=$(which nginx)
+#read domain
+#echo "$domain.com $domain installation directory $d"
+
+
+#remove .com from example.com
+sed -i "$ip  $domain.com venus" /etc/hosts
+
+
+#downloading wordpress and ensuring if curl is installed
+
+dpkg --get-selections | grep curl
+if [ `echo $?` -eq 1 ]
+then
+echo "+----------------------------------------+"
+echo -e "instaling curl before downloading wordpress"
+echo "+----------------------------------------+"
+sudo apt install -y curl
+else
+curl -O https://wordpress.org/latest.tar.gz
+fi
+done
