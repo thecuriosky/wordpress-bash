40fa6f2c5230f79d3e44e1c03ccc1a424b19eed0
thecuriosky <48576841+thecuriosky@users.noreply.github.com>
2019-04-05 21:09:24 +0530

    Update wordpress-install.sh
    making sed work in bash script took so many hours, moving forward with new line of code.

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#use alt shift 3 to view line number in nano editor
 #Check if PHP, Mysql & Nginx are installed. If not present, install the missing packages
 
 for i in nginx mysql php
@@ -56,11 +57,57 @@ done
 sudo systemctl restart php7.2-fpm
 
 
-
 #editing nginx config file for wordpress
-sudo nano /etc/nginx/sites-available/akashmhaske.com
+#1. creating a nginx file in /etc/nginx/sites-available/
 
+#2. replacing example.com with $domain.com #sudo -s cat <<EOT >> "/etc/nginx/sites-available/$domain.com"
+sudo -s touch /etc/nginx/sites-available/$domain.com
+sudo sh -c "cat >> /etc/nginx/sites-available/$domain.com <<'EOT'
+server {
+        listen 80;
+        root /var/www/html;
+        index index.php index.html index.htm index.nginx-debian.html;
+        server_name example.com;
 
+        location / {
+                try_files $uri $uri/ =404;
+        }
+
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
+        }
+
+        location ~ /\.ht {
+                deny all;
+        }
+}
+EOT" | sudo sed -i "s/server_name example.com;/server_name $domain.com;/g" "/etc/nginx/sites-available/$domain.com"
+#-------------------------------------------------------------------------------------------------
+#Enable your new server block by creating a symbolic link from your new server block configuration file
+sudo ln -s /etc/nginx/sites-available/$domain.com /etc/nginx/sites-enabled/
+sudo unlink /etc/nginx/sites-enabled/default
+#Note: If you ever need to restore the default configuration, you can do so by recreating the symbolic link, like this:
+#sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+#Test your new configuration file for syntax errors:
+sudo nginx -t
+#reload nginx
+sudo systemctl reload nginx
+
+#configuring php
+#sudo -s touch /var/www/html/info.php
+#sudo sh -c "cat >> /var/www/html/info.php <<'EOT'
+#<?php
+#phpinfo();
+#EOT"
+
+#creating mysql database
+sudo mysql
+echo "enter root password when prompted"
+echo ""
+mysql -u root -p
+
+-----------------
 dpkg --get-selections | grep curl
 if [ `echo $?` -eq 1 ]
 then
@@ -69,6 +116,8 @@ echo -e "instaling curl before downloading wordpress"
 echo "+----------------------------------------+"
 sudo apt install -y curl
 else
+echo "Downloading wordpress"
 curl -O https://wordpress.org/latest.tar.gz
 fi
 done
+
